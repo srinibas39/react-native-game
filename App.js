f610229223cc1c3e321c61a68a94fba423447168
srinibas <srinibaskhuntia39@gmail.com>
2022-07-10 18:21:30 +0530

    added background Image

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
 import { StartGame } from './screen/StartGame';
 import { LinearGradient } from 'expo-linear-gradient';
 
@@ -7,6 +7,8 @@ export default function App() {
   return (
     <LinearGradient colors={["#f1f5f9", "#dc2626"]} style={styles.appContainer} >
       <StartGame />
+      <ImageBackground source={require("./assets/images/dice2.jpg")} resizeMode="cover" style={styles.ImageBackground}>
+      </ImageBackground>
     </LinearGradient >
   );
 }
@@ -16,6 +18,10 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 16,
     flexDirection: "column",
+  },
+  ImageBackground: {
+    flex: 1,
+    opacity: 0.15
   }
 })
 
