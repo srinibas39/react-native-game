20c2da0cc462b791233bac1eca06621417bf370f
srinibas <srinibaskhuntia39@gmail.com>
2022-07-11 00:15:01 +0530

    game logic completed

--- screen/Game.js
+++ screen/Game.js
@@ -1,26 +1,65 @@
 import { useState } from "react"
-import { Text, View } from "react-native"
+import { Text, View, Alert } from "react-native"
 import { GuessedNumber } from "../components/GuessedNumber"
+import { PrimaryButton } from "../components/PrimaryButton"
 import { Title } from "../components/Title"
 
 
 
-export const Game = ({enteredNo}) => {
+export const Game = ({ enteredNo }) => {
 
-    const generateNo=(min,max,exclude)=>{
-         const randNo= Math.floor(Math.random()*(max-min))+min;
-         if(randNo===exclude){
-            return generateNo(min,max,exclude)
-         }
-         else{
+    let min = 1;
+    let max = 100;
+
+    const generateNo = (min, max, exclude) => {
+        const randNo = Math.floor(Math.random() * (max - min)) + min;
+        if (randNo === exclude) {
+            return generateNo(min, max, exclude)
+        }
+        else {
             return randNo;
-         }
+        }
     }
-    const initalGuess=generateNo(1,100,enteredNo)
-    const [guessedNo,setGuessedNo]=useState(initalGuess)
+    
+    const initalGuess = generateNo(min, max, enteredNo)
+    const [guessedNo, setGuessedNo] = useState(initalGuess)
+
+  
+
+    const guessHighOrLower = (type) => {
+
+        if (type === "lower" && guessedNo < enteredNo || type === "higher" && guessedNo > enteredNo) {
+            Alert.alert(
+                "Invalid option",
+                "Please do appropriate operation",
+                [{ text: "Ok", style: "destructive" }]
+            )
+        }
+        if (type === "lower") {
+            max = enteredNo;
+        }
+        else if (type === "higher") {
+            min = enteredNo + 1;
+        }
+
+        const newRandomNo = generateNo(min, max, enteredNo);
+        setGuessedNo(newRandomNo)
+    }
+
+
 
     return <View>
         <Title>Your Guess!!!</Title>
         <GuessedNumber>{guessedNo}</GuessedNumber>
+        <View>
+            <View>
+                <Text>Higher or Lower?</Text>
+            </View>
+            <View>
+                <PrimaryButton handlePress={() => guessHighOrLower("lower")}>-</PrimaryButton>
+                <PrimaryButton handlePress={() => guessHighOrLower("higher")}>+</PrimaryButton>
+            </View>
+
+        </View>
     </View>
 }
