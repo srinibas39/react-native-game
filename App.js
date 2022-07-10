312a1eebba9261ed643b669b9e1ce536681ea3a9
srinibas <srinibaskhuntia39@gmail.com>
2022-07-10 13:57:10 +0530

    linear gradient

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import { StartGame } from './screen/StartGame';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
-    <View style={styles.appContainer} >
-      <StatusBar />
+    <LinearGradient colors={["#f1f5f9", "#dc2626"]} style={styles.appContainer} >
       <StartGame />
-    </View>
+    </LinearGradient >
   );
 }
 
@@ -15,7 +15,7 @@ const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
     padding: 16,
-    flexDirection: "column"
+    flexDirection: "column",
   }
 })
 
--- screen/StartGame.js
+++ screen/StartGame.js
@@ -3,35 +3,45 @@ import { PrimaryButton } from "../components/PrimaryButton"
 
 export const StartGame = () => {
   return <View style={styles.inputContainer}>
-    <TextInput style={styles.input} maxLength={2} keyboardType="number-pad"/>
-    <PrimaryButton>Reset</PrimaryButton>
-    <PrimaryButton>Confirm</PrimaryButton>
+    <View style={styles.inputBox}>
+      <TextInput style={styles.input} maxLength={2} keyboardType="number-pad" />
+    </View>
+    <View>
+      <PrimaryButton >Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
   </View>
 }
 
 const styles = StyleSheet.create({
   inputContainer: {
-    padding:16,
-    marginVertical:16,
-    borderRadius:5,
-    backgroundColor:"#f1f5f9",
+    padding: 16,
+    marginVertical: 16,
+    borderRadius: 5,
+    backgroundColor: "#fee2e2",
     // shadow for android
-    elevation:4,
+    elevation: 4,
     // shadow for ios
-    shadowColor:"black",
-    shadowRadius:5,
-    shadowOffset:{width:0,height:2},
-    shadowOpacity:0.25 
+    shadowColor: "black",
+    shadowRadius: 5,
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25
   },
   input: {
-    width:100,
-    fontSize:32,
-    fontWeight:"bold",
-    borderWidth:2,
-    padding:8,
-    margin:8,
-    borderColor:"#dc2626",
-    color:"#dc2626",
-    textAlign:"center"
-  }
+    width: 100,
+    fontSize: 32,
+    fontWeight: "bold",
+    borderWidth: 2,
+    padding: 8,
+    margin: 8,
+    borderColor: "#dc2626",
+    color: "#dc2626",
+    textAlign: "center"
+  },
+  inputBox:{
+    flexDirection:"row",
+    justifyContent:"center",
+    alignItems:"center"
+  },
+
 })
